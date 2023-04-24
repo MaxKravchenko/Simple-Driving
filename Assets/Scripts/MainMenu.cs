9c8546efb4e9a76e7d545ddda7e5010f19484a31
Maxim Kravchenko <max.kravchenko.dev@gmail.com>
2023-04-24 21:41:25 +0300

    iOS Notifications

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,7 +10,8 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private TMP_Text highScoreText;
     [SerializeField] private TMP_Text energyText;
     [SerializeField] private AndroidNotificationHandler androidNotificationHandler;
-    [SerializeField] private int maxEnergy;
+    [SerializeField] private IOSNotificationHandler iosNotificationHandler;
+    [SerializeField] private int maxEnergy; 
     [SerializeField] private int energyRechargeDuration;
 
     private int energy;
@@ -58,6 +59,8 @@ public class MainMenu : MonoBehaviour
             PlayerPrefs.SetString(EnergyReadyKey, energyReady.ToString());
 #if UNITY_ANDROID
             androidNotificationHandler.ScheduleNotification(energyReady);
+#elif UNITY_IOS
+            iosNotificationHandler.ScheduleNotification(energyRechargeDuration);
 #endif
         }
 
